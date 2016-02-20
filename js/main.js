62cf61e804852a9a2d39d45addb7922498fd0650
David Leger <davidleger95@me.com>
2016-02-20 14:05:22 -0400

    new commit

--- js/main.js
+++ js/main.js
@@ -103,7 +103,82 @@
                     changeOpacity(o);
                 }
             }else if(inputScheme == 3){
-
+                if(event.keyCode == 87){
+                    r += 5;
+                    rotateObject();
+                }else if(event.keyCode == 83){
+                    r -= 5;
+                    rotateObject();
+                }else if(event.keyCode == 82){
+                    c++;
+                    if(c > 11){
+                        c=0;
+                    }
+                    changeColor(c);
+                }else if(event.keyCode == 49){
+                    c = 0;
+                    changeColor(c);
+                }else if(event.keyCode == 50){
+                    c = 1;
+                    changeColor(c);
+                }else if(event.keyCode == 51){
+                    c = 2;
+                    changeColor(c);
+                }else if(event.keyCode == 52){
+                    c = 3;
+                    changeColor(c);
+                }else if(event.keyCode == 53){
+                    c = 4;
+                    changeColor(c);
+                }else if(event.keyCode == 54){
+                    c = 5;
+                    changeColor(c);
+                }else if(event.keyCode == 55){
+                    c = 6;
+                    changeColor(c);
+                }else if(event.keyCode == 56){
+                    c = 7;
+                    changeColor(c);
+                }else if(event.keyCode == 57){
+                    c = 8;
+                    changeColor(c);
+                }else if(event.keyCode == 48){
+                    c = 9;
+                    changeColor(c);
+                }else if(event.keyCode == 189){
+                    c = 10;
+                    changeColor(c);
+                }else if(event.keyCode == 187){
+                    c = 11;
+                    changeColor(c);
+                }else if(event.keyCode == 65){
+                    if(o > 0){
+                        o -= 0.1;
+                    }
+                    changeOpacity(o);
+                }
+                else if(event.keyCode == 68){
+                    if(o < 1){
+                        o += 0.1;
+                    }
+                    changeOpacity(o);
+                }
+                
+                if(event.keyCode == 38){
+                    y -= 5;
+                    $(currObject).css({'top': y});
+                }else if(event.keyCode == 40){
+                    y += 5;
+                    $(currObject).css({'top': y});
+                }
+                
+                if(event.keyCode == 37){
+                    x -= 5;
+                    $(currObject).css({'left': x});
+                }else if(event.keyCode == 39){
+                    x += 5;
+                    $(currObject).css({'left': x});
+                }
             }
         });
         
